466adbc79339d9e1314accc8ba3d0bec3cdeb303
Basitkhan <abasitkk92@gmail.com>
2023-09-11 17:24:11 +0500

    Pages and theircorresponding css modules designed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AppLayout from "./pages/AppLayout";
+import HomePage from "./pages/HomePage";
+import PageNotFound from "./pages/PageNotFound";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
+
 export default function App() {
-  const x = 23;
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
